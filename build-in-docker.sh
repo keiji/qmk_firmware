b1dc8fda6afe98a64403ef21372457c934ed7ee7
vvakame <vvakame+dev@gmail.com>
2016-05-07 23:28:21 +0900

    add build-in-docker.sh

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -3,5 +3,8 @@
 # If you want build mannualy, exec below command.
 # docker run -i -t -v $(pwd):/qmk_firmware vvakame/build-qmk_firmware /bin/bash
 
-docker run -t --rm -v $(pwd):/qmk_firmware vvakame/build-qmk_firmware /bin/bash -ci "cd /qmk_firmware/keyboard/ergodox_ez && make KEYMAP=vv"
+docker run -t --rm -v $(pwd):/qmk_firmware vvakame/build-qmk_firmware /bin/bash \
+	-ci "cd /qmk_firmware/keyboard/ergodox_ez && make KEYMAP=vv"
+	# -ci "cd /qmk_firmware/keyboard/ergodox_ez && make clean && make KEYMAP=vv CONSOLE_ENABLE=yes"
 ls -la $(pwd)/keyboard/ergodox_ez/ergodox_ez.hex
+cp $(pwd)/keyboard/ergodox_ez/ergodox_ez.hex $(pwd)
